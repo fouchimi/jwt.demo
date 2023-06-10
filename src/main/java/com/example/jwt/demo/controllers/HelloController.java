c667a644cd197bef25a71b9f6c6eae67654e8732
Fouchimi Nvuh Ousmane <fousmane@Fouchimis-Air.lan>
2023-06-10 10:13:53 -0400

    Added extra exclamation point

--- src/main/java/com/example/jwt/demo/controllers/HelloController.java
+++ src/main/java/com/example/jwt/demo/controllers/HelloController.java
@@ -9,6 +9,6 @@ public class HelloController {
 
     @RequestMapping("hello")
     public String helloWorld(@RequestParam(value="name", defaultValue="World") String name) {
-        return "Hello "+name+"!!";
+        return "Hello "+name+"!!!";
     }
 }
